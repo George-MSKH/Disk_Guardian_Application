063bac7c37e9fa8c6923dc44724418bb13ea7db1
George <user@users-MacBook-Pro.local>
2026-02-20 16:07:22 +0400

    Add features to check CPU and MEMORY also updated README based on changes

--- app/guardian.py
+++ app/guardian.py
@@ -1,6 +1,7 @@
 import psutil
 import time
 import os
+import json 
 import requests
 from dotenv import load_dotenv
 
@@ -11,7 +12,11 @@ load_dotenv()
 # ==============================
 
 CHECK_PATH = "/host" if os.path.exists("/host") else "/"
-THRESHOLD = float(os.getenv("DISK_THRESHOLD", "80.0"))
+THRESHOLD = {
+    "disk": float(os.getenv("DISK_THRESHOLD", "80.0")),
+    "cpu": float(os.getenv("CPU_THRESHOLD", "75.0")),
+    "memory": float(os.getenv("MEM_THRESHOLD", "80.0"))
+}
 CHECK_INTERVAL = 10  # seconds
 ALERT_COOLDOWN = 300  # seconds (5 minutes)
 
@@ -43,50 +48,171 @@ def send_alert(message):
 
 
 # ==============================
+# Log setup
+# ==============================
+
+
+HEARTBEAT_LOG_FILE = f"logs/heartbeat_log_{time.strftime('%Y-%m-%d_%H-%M-%S')}.json"
+ALERT_LOG_FILE = f"logs/alert_log_{time.strftime('%Y-%m-%d_%H-%M-%S')}.json"
+
+# ==============================
+# Logging functions
+# ==============================
+
+def log_heartbeat(resource, usage_percent, free_gb=None):
+
+    entry = {
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "resource": resource,
+        "usage_percent": usage_percent,
+        "free_gb": free_gb
+    }
+
+    try:
+        with open(HEARTBEAT_LOG_FILE, "r") as f:
+            logs = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        logs = []
+    logs.append(entry)
+    with open (HEARTBEAT_LOG_FILE, "w") as f:
+        json.dump(logs, f, indent=2)
+
+def log_alert(resource, usage_percent, free_gb=None, alert_type="HIGH"):
+    entry = {
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "resource": resource,
+        "usage_percent": usage_percent,
+        "free_gb": free_gb,
+        "alert": alert_type
+    }
+
+    try:
+        with open(ALERT_LOG_FILE, "r") as f:
+            logs = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        logs = []
+    logs.append(entry)
+    with open (ALERT_LOG_FILE, "w") as f:
+        json.dump(logs, f, indent=2)
+
+        
+# ==============================
+# Resource check functions
+# ==============================
+        
+def check_disk():
+    stats = psutil.disk_usage(CHECK_PATH)
+    percent = stats.percent
+    free_gb = stats.free // (2**30)
+    return percent, free_gb
+
+def check_cpu():
+    percent = psutil.cpu_percent(interval=1)
+    return percent
+
+def check_memory():
+    mem = psutil.virtual_memory()
+    percent = mem.percent
+    free_gb = mem.available // (2**30)
+    return percent, free_gb
+
+# ==============================
 # Monitoring Logic
 # ==============================
 
-def monitor_disk():
+def monitor_resources():
     print(f"--- Monitoring {CHECK_PATH} (Threshold: {THRESHOLD}%) ---")
 
-    last_alert_time = 0
-    alert_active = False  # Tracks whether we're currently in alert state
+    last_alert_time = {
+        "disk": 0,
+        "cpu": 0,
+        "memory": 0
+    }
+
+    alert_active = {
+        "disk": False,
+        "cpu": False,
+        "memory": False
+    } 
 
     while True:
-        try:
-            stats = psutil.disk_usage(CHECK_PATH)
-        except Exception as e:
-            print(f"Disk check failed: {e}")
-            time.sleep(CHECK_INTERVAL)
-            continue
-
-        current_percent = stats.percent
-        free_gb = stats.free // (2**30)
-
-        print(f"[Heartbeat] Usage: {current_percent}% | Free: {free_gb}GB")
-
         now = time.time()
 
-        # ==============================
-        # Alert Trigger
-        # ==============================
-        if current_percent > THRESHOLD:
-            if not alert_active or (now - last_alert_time > ALERT_COOLDOWN):
-                msg = f"Disk usage HIGH on {CHECK_PATH}: {current_percent}%"
+        #---------------------
+        # DISK CHECK
+        #---------------------
+
+        disk_percent, disk_free = check_disk()
+        print(f"[Disk] Usage: {disk_percent}% | Free: {disk_free}GB")
+        log_heartbeat("disk", disk_percent, free_gb=disk_free)
+
+        if disk_percent > THRESHOLD["disk"]:
+            if not alert_active["disk"] or (now - last_alert_time["disk"] > ALERT_COOLDOWN):
+                msg = f"Disk usage HIGH on {CHECK_PATH}: {disk_percent}%"
                 print(f"!!! {msg}")
                 send_alert(msg)
-                last_alert_time = now
-                alert_active = True
-
-        # ==============================
-        # Recovery Detection
-        # ==============================
-        else:
-            if alert_active:
-                msg = f"Disk usage RECOVERED on {CHECK_PATH}: {current_percent}%"
+                log_alert("disk", disk_percent, free_gb=disk_free, alert_type="HIGH")
+                last_alert_time["disk"] = now
+                alert_active["disk"] = True
+        else: 
+            if alert_active["disk"]:
+                msg = f"Disk usage RECOVERED on {CHECK_PATH}: {disk_percent}%"
                 print(f"*** {msg}")
                 send_alert(msg)
+                log_alert("disk", disk_percent, free_gb=disk_free, alert_type="RECOVERED")
                 alert_active = False
+
+        #---------------------
+        # CPU CHECK
+        #---------------------
+
+        cpu_percent = check_cpu()
+        print(f"[CPU] Usage: {cpu_percent}%")
+        log_heartbeat("cpu", cpu_percent)
+
+        if cpu_percent > THRESHOLD["cpu"]:
+            if not alert_active["cpu"] or (now - last_alert_time["cpu"] > ALERT_COOLDOWN):
+                msg = f"[CPU] Usage HIGH: {cpu_percent}%"
+                print(f"!!! {msg}")
+                send_alert(msg)
+                log_alert("cpu", cpu_percent, alert_type="HIGH")
+                last_alert_time["cpu"] = now
+                alert_active = True
+        else:
+            if alert_active["cpu"]:
+                msg = f"CPU usage RECOVERED: {cpu_percent}%"
+                print(f"*** {msg}")
+                send_alert(msg)
+                log_alert("cpu", cpu_percent, alert_type="RECOVERED")
+                alert_active = False
+
+        #---------------------
+        # MEMORY CHECK
+        #---------------------
+
+        mem_percent, mem_free = check_memory()
+        print(f"[Memory] Usage: {mem_percent}% | Free {mem_free}GB")
+        log_heartbeat("memory", mem_percent, free_gb=mem_free)
+
+        if mem_percent > THRESHOLD["memory"]:
+            if not alert_active["memory"] or (now - last_alert_time["memory"] > ALERT_COOLDOWN):
+                msg = f"Memory usage HIGH: {mem_percent}%"
+                print(f"!!! {msg}")
+                send_alert(msg)
+                log_alert("memory", mem_percent, free_gb=mem_free, alert_type="HIGH")
+                last_alert_time["memory"] = now
+                alert_active = True
+        else:
+            if alert_active["memory"]:
+                msg = f"Memory usage RECOVERED: {mem_percent}%"
+                print(f"*** {msg}")
+                send_alert(msg)
+                log_alert("memory", mem_percent, free_gb=mem_free, alert_type="RECOVERED")
+                alert_active = False
+                
+        # --------------------
+        # Wait for next check
+        # --------------------
 
         time.sleep(CHECK_INTERVAL)
 
@@ -96,4 +222,4 @@ def monitor_disk():
 # ==============================
 
 if __name__ == "__main__":
-    monitor_disk()
+    monitor_resources()
